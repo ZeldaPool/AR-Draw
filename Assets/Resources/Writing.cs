9a33b7a043fed3a0df6cc300578b1441cbef5723
ZeldaPool <smitrana007@gmail.com>
2019-08-27 18:01:00 +0530

    Initial commit

---
+++ Assets/Resources/Writing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Writing : MonoBehaviour
+{
+
+public GameObject paint;
+public GameObject imageTarget;
+
+void Update()
+{
+       
+    if (Input.touchCount > 0)
+    {
+        imageTarget = GameObject.Find("Board");
+        GameObject clone = Instantiate(paint, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation) as GameObject;
+        clone.transform.parent = imageTarget.transform;
+    }
+}
+
+
+}
